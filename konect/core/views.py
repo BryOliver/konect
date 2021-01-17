c6455773f8b605180ba777f223423ed650c50e35
Bryan Sousa <bryan.oliver999@gmail.com>
2021-01-17 19:07:44 -0300

    08 - colocando o placeholder no formulário da página inicial

--- konect/core/views.py
+++ konect/core/views.py
@@ -16,6 +16,15 @@ class Index(CreateView):
         context['cadastro'] = Cadastro.objects.last()
         return context
 
+    def get_form(self, form_class=None):
+        if form_class is None:
+            form_class = self.get_form_class()
+
+        form = super(Index, self).get_form(form_class)
+        form.fields['nome'].widget.attrs = {'placeholder': 'Nome Completo'}
+        form.fields['email'].widget.attrs = {'placeholder': 'E-mail'}
+        return form
+
 def categoria_view(request, slug):
     categoria = get_object_or_404(Categoria, slug=slug)
     areas = Area.objects.filter(categoria=categoria)
